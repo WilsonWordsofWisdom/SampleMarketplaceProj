beaecaac8f21ff5631a64b069c76caee991745a1
Peter Tang <Peter_Tang@Singaporeair.com.sg>
2019-12-10 16:03:46 +0800

    test files

--- items.py
+++ items.py
@@ -7,11 +7,11 @@ class Items:
 
     def add(self, item):
         self.df = self.df.append(pd.Series(item),ignore_index=True)
-        self.df.to_csv('Products.csv', index=False)
+        #self.df.to_csv('Products.csv', index=False)
 
     def get_items(self, filter_names=None):
         if filter_names is None:
             return self.df
         else:
-            return df.loc[filter_names]
+            return self.df.loc[filter_names]
         
