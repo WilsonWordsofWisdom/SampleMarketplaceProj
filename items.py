404eff9d90ebe13e78ea6793978add5360af7256
Peter Tang <Peter_Tang@Singaporeair.com.sg>
2019-12-10 16:48:30 +0800

    add function finished

--- items.py
+++ items.py
@@ -1,15 +1,28 @@
 import pandas as pd
+from icecream import ic
+from loguru import logger
+
+
+
+
+ic.configureOutput(outputFunction=lambda s: logger.debug(s))
+ic.configureOutput(includeContext=True)
+ic.configureOutput(prefix="")
 
 class Items:
 
     def __init__(self):
         self.df = pd.read_csv('Products.csv')
+        ic(self.df)
 
-    def add(self, item, save=True):
+    def add(self, item):
+        ic(self.df)
+        print(item)
+        ic(item)
         self.df = self.df.append(pd.Series(item),ignore_index=True)
-        if save:
-            self.df.to_csv('Products.csv', index=False)
-
+        #self.df.to_csv('Products.csv', index=False)
+        ic(self.df)
+        return self.df
     def get_items(self, filter_names=None):
         if filter_names is None:
             return self.df
