a907340a9a8c8d1acf6d9b0006858091e835b9ae
Peter Tang <Peter_Tang@Singaporeair.com.sg>
2019-12-10 14:40:05 +0800

    read csv

--- items.py
+++ items.py
@@ -1,7 +1,7 @@
 import pandas as pd
 class Items:
     def __init__(self):
-        self.df = pd.DataFrame([('book', '1'), ('ipad', 4)], columns=['name', 'price'])
+        self.df = pd.read_csv('Products.csv') #pd.DataFrame([('book', '1'), ('ipad', 4)], columns=['name', 'price'])
     def add(self, item):
         self.df = self.df.append(pd.Series(item),ignore_index=True)
     def get_items(self, fitler=None):
