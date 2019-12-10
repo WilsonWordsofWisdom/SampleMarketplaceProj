7bc37d1b4e18db3aecf281c3df475311a2f24e52
Peter Tang <Peter_Tang@Singaporeair.com.sg>
2019-12-10 15:08:06 +0800

    flask example

--- flask_container.py
+++ flask_container.py
@@ -21,13 +21,13 @@ ic.configureOutput(prefix="")
 ####################
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+#cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 items = Items()
 ##########################
 # todo: change as needed
 # can also write in train class and import to here
 # invocations functions, as this function subject to highly frequent changes, just keep it here
-@app.route("/invocations", methods=["POST"])
+@app.route('/')
 def predict():
     data = request.get_json(force=True)
     return Items.get_items().to_json()
